6196c8506a94965351e29679debb81da05d17575
Asish K Antony <asishantony@gmail.com>
2023-07-04 17:05:00 +0530

    Prune Databases

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                //   'status_code' => 401,
+                  'success' => 0,
+                  'message' => 'Unauthenticated.'
+                ], 401);
+            }
+           });
     }
 }
