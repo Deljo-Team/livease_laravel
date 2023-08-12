127af8d26667cc8255a2d783ea3b19d2b8892aae
Asish K Antony <asishantony@gmail.com>
2023-08-12 09:29:22 +0530

    servicemen section

--- database/migrations/2023_08_10_085257_create_servicemens_table.php
+++ database/migrations/2023_08_10_085257_create_servicemens_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone')->unique();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->foreignId('subcategory_id')->constrained('subcategories')->onDelete('cascade');
+            $table->foreignId('sub_category_id')->constrained('sub_categories');
             $table->boolean('is_available')->default(true);
             $table->boolean('is_verified')->default(false);
             $table->string('id_proof')->nullable();
