d67f63d6de47ff2595ea1df0de6ae14f903f2705
Asish K Antony <asishantony@gmail.com>
2023-10-06 23:31:58 +0530

    LIV 23

---
+++ database/migrations/2023_10_05_080059_create_sub_category_questions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_category_questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sub_category_id')->constrained('sub_categories')->onDelete('cascade');
+            $table->enum('type', ['select', 'text', 'date', 'time', 'file', 'boolean']);
+            $table->string('question');
+            $table->string('answer')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_category_questions');
+    }
+};
