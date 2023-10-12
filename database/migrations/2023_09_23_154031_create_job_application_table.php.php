e35a7554928d3876f978dfd8119f52be73d07bcc
deljoCS <deljo@composolution.com>
2023-10-12 12:32:29 +0530

    change to enum

--- database/migrations/2023_09_23_154031_create_job_application_table.php.php
+++ database/migrations/2023_09_23_154031_create_job_application_table.php.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('linked_in_link')->nullable();
             $table->date('visa_validity')->nullable();
             $table->string('gender')->nullable();
+            $table->enum('gender', ['male', 'female']);
             $table->foreignId('job_type_id')->constrained('job_type')->onDelete('cascade');
             $table->enum('experience_level', ['experienced', 'fresher']);  
             $table->foreignId('current_job_id')->constrained('job')->onDelete('cascade');
